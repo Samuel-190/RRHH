b23327cf18e7b551d7cf9bb68f5a2595b2cbbe65
samuel lopez <lopezosoriosemil@gmail.com>
2026-03-09 12:26:14 -0500

    refactor(contracts) Restructurar el sistema de rutas para mayor eficiencia y escalabilidad.

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,16 @@
 <?php
 use App\Http\Controllers\CollaboratorController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContractController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('collaborators', CollaboratorController::class)
-    ->middleware(['auth', 'role:gestor_rrhh']);
+Route::middleware(['auth','role:gestor_rrhh'])->group(function () {
+
+    Route::resource('collaborators', CollaboratorController::class);
+    Route::resource('contracts', ContractController::class);
+
+}); 
 
